b84f37785fe68409421467d825016cd75d84f041
NEO007x2 <neonat0896684779@gmail.com>
2026-02-07 09:11:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -370,7 +370,7 @@ function renderGallery() {
   } else {
     galleryGrid.innerHTML = photos.map((photo, index) => `
       <div class="gallery-item">
-        <img src="${photo.url}" alt="Captured at ${photo.date}">
+        <img src="${photo.url}" alt="Captured at ${photo.date}" style="transform: rotate(180deg);">
         <button class="delete-photo-btn" onclick="deletePhoto(${index})" title="Delete photo">×</button>
       </div>
     `).join('');
